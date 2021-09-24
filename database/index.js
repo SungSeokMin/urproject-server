a2aac4a75aeab9711826c08187bb3cd5c5d76ffe
SungSeokMin <jkl154527@gmail.com>
2021-09-24 12:35:12 +0900

    🚀 env 설정

--- database/index.js
+++ database/index.js
@@ -1,11 +1,12 @@
 const { createPool } = require('mysql2/promise');
+require('dotenv').config();
 
 async function connect() {
   const connection = await createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '1234',
-    database: 'urproject',
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
   });
   return connection;
 }
--- multer.js
+++ multer.js
@@ -9,7 +9,7 @@ const s3 = new aws.S3();
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: 'urproject-image',
+    bucket: 'urproject-images',
     acl: 'public-read',
     key: function (req, file, cb) {
       cb(
